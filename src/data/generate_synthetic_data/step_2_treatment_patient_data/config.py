60af33744b7e4b2205e8e000570c02134f02a322
Ryndova Irina <ryndovaira@gmail.com>
2024-12-26 15:41:18 +0300

    rename file

--- src/data/generate_synthetic_data/step_2_treatment_patient_data/config.py
+++ src/data/generate_synthetic_data/step_2_treatment_patient_data/config.py
@@ -1,2 +1,2 @@
 TEST_MODE = True  # Set to True for testing or False for full data generation
-TEST_LIMIT = 5  # Number of records to process in test mode
+TEST_LIMIT = 1  # Number of records to process in test mode
